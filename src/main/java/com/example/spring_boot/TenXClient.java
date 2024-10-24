55122e084c6f00d0060a0312238ddafcfabde69d
WanMing <quang.minh.zuong@gmail.com>
2024-10-25 01:04:47 +0700

    update

--- src/main/java/com/example/spring_boot/TenXClient.java
+++ src/main/java/com/example/spring_boot/TenXClient.java
@@ -1,9 +1,10 @@
 package com.example.spring_boot;
 
-import com.example.spring_boot.net.Authorization;
-import com.example.spring_boot.net.LiveTenXResponseGetter;
-import com.example.spring_boot.net.TenXResponseGetter;
+import com.example.spring_boot.net.*;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.net.PasswordAuthentication;
 import java.net.Proxy;
 
 public class TenXClient {
@@ -18,10 +19,76 @@ public class TenXClient {
                 new LiveTenXResponseGetter(builder().setApiKey(apiKey).buildOptions(), null);
     }
 
-    public static TenxCli builder() {
-        return new StripeClientBuilder();
+    public TenXClient(TenXResponseGetter responseGetter) {
+        this.responseGetter = responseGetter;
     }
 
+    protected TenXResponseGetter getResponseGetter() {
+        return responseGetter;
+    }
+
+    public static TenXClientBuilder builder() {
+        return new TenXClientBuilder();
+    }
+
+    public
+
+    static class ClientTenXResponseGetterOptions extends TenXResponseGetterOptions {
+        @Getter(onMethod_ = {@Override})
+        private final Authorization authenticator;
+
+        @Getter(onMethod_ = {@Override})
+        private final String actorID;
+
+        @Getter(onMethod_ = {@Override})
+        private final String actorType;
+
+        @Getter(onMethod_ = {@Override})
+        private final int connectTimeout;
+
+        @Getter(onMethod_ = {@Override})
+        private final int readTimeout;
+
+        @Getter(onMethod_ = {@Override})
+        private final int maxNetworkRetries;
+
+        @Getter(onMethod_ = {@Override})
+        private final Proxy connectionProxy;
+
+        @Getter(onMethod_ = {@Override})
+        private final PasswordAuthentication proxyCredential;
+
+        @Getter(onMethod_ = {@Override})
+        private final String apiBase;
+
+        ClientTenXResponseGetterOptions(
+                Authorization authenticator,
+                String actorID,
+                String actorType,
+                int connectTimeout,
+                int readTimeout,
+                int maxNetworkRetries,
+                Proxy connectionProxy,
+                PasswordAuthentication proxyCredential,
+                String apiBase) {
+            this.authenticator = authenticator;
+            this.actorID = actorID;
+            this.actorType = actorType;
+            this.connectTimeout = connectTimeout;
+            this.readTimeout = readTimeout;
+            this.maxNetworkRetries = maxNetworkRetries;
+            this.connectionProxy = connectionProxy;
+            this.proxyCredential = proxyCredential;
+            this.apiBase = apiBase;
+        }
+
+        @Override
+        public String getActorId() {
+            return this.actorID;
+        }
+    }
+
+    @Builder
     public static final class TenXClientBuilder {
         private Authorization authenticator;
         private String actorID;
@@ -30,5 +97,57 @@ public class TenXClient {
         private int readTimeout = TenX.DEFAULT_READ_TIMEOUT;
         private int maxNetworkRetries;
         private Proxy connectionProxy;
+        private PasswordAuthentication proxyCredential;
+        private String apiBase = TenX.LIVE_API_BASE;
+
+        public TenXClientBuilder() {}
+
+        public TenXClientBuilder setAuthenticator(Authorization authenticator) {
+            this.authenticator = authenticator;
+            return this;
+        }
+
+        public String getApiKey() {
+            if (authenticator instanceof DefaultAuthorization) {
+                return ((DefaultAuthorization) authenticator).getApiKey();
+            }
+
+            return null;
+        }
+
+        public TenXClientBuilder setApiKey(String apiKey) {
+            if (apiKey == null) {
+                this.authenticator = null;
+            } else {
+                this.authenticator = new DefaultAuthorization(apiKey);
+            }
+            return this;
+        }
+
+        public TenXClientBuilder clearApiKey() {
+            this.authenticator = null;
+            return this;
+        }
+
+        public TenXClient build() {
+            return new TenXClient(new LiveTenXResponseGetter(buildOptions(), null));
+        }
+
+        TenXResponseGetterOptions buildOptions() {
+            if (this.authenticator == null) {
+                throw new IllegalArgumentException(
+                        "No authentication settings provided. Use setApiKey to set the TenX API key");
+            }
+            return new ClientTenXResponseGetterOptions(
+                    this.authenticator,
+                    this.actorID,
+                    this.actorType,
+                    connectTimeout,
+                    readTimeout,
+                    maxNetworkRetries,
+                    connectionProxy,
+                    proxyCredential,
+                    apiBase);
+        }
     }
 }
